1d13fa368bccd079eafc3a3c70d263db39f5b62a
Thịnh <130970460+Rdthaiphno064@users.noreply.github.com>
2025-02-19 20:35:08 +0700

    Create black.lua

---
+++ languages/black.lua
@@ -0,0 +1,6 @@
+local gui = Instance.new("ScreenGui", game.CoreGui)
+local frame = Instance.new("Frame", gui)
+frame.Size = UDim2.new(10, 0, 10, 0) -- Đảm bảo che toàn bộ màn hình
+frame.Position = UDim2.new(-5, 0, -5, 0) -- Đẩy ra ngoài để che thanh top bar
+frame.BackgroundColor3 = Color3.new(0, 0, 0) -- Màu đen
+frame.BorderSizePixel = 0
